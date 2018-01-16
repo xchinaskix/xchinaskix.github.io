ac86b5437d4dc78d01a84c75934bbaff2b562371
xchinaskix <jglushko93@gmail.com>
2018-01-16 22:26:33 +0200

    4

--- main.js
+++ main.js
@@ -25,8 +25,7 @@ window.onload = function () {
     }
 
     showAdressBook();
-
-
+	
     //EVENTLISTENER
     quickAdd.addEventListener('click', function () {
         console.log('button is clicked');
@@ -42,11 +41,12 @@ window.onload = function () {
 
     addButton.addEventListener('click', addtoBook);
 
-
     function addtoBook() {
-        var isNull = firstName.value != '' && secondName.value != '' && phone.value != '' && email.value != '';
-        console.log(isNull);
-        if (isNull) {
+		valName();
+		valPhone();
+		valSName();
+		valEmail();
+		if (valName() && valPhone() && valSName() && valEmail() ) {
             //add the content from the form to arr and local storage
             var obj = new jSonStructure(firstName.value.toUpperCase(), secondName.value.toUpperCase(), phone.value, email.value);
             addressBook.push(obj);
@@ -59,14 +59,14 @@ window.onload = function () {
             showAdressBook();
         }
     }
-
+	//очистка формы 
     function clearForm() {
         var frm = document.querySelectorAll('.formFields');
         for (i in frm) {
             frm[i].value = '';
         }
     }
-
+	//удаление контакта
     function removeEntry(event) {
         if (event.target.classList.contains("delbutton")) {
             var remID = event.target.getAttribute("data-id");
@@ -77,7 +77,7 @@ window.onload = function () {
             console.log('function must worked');
         }
     }
-	
+	//редактирование контакта
 	function editEntry(event) {
         if (event.target.classList.contains("editbutton")) {
             var remID = event.target.getAttribute("data-id");
@@ -93,10 +93,8 @@ window.onload = function () {
 		}
     }
 
-
+	//отображение списка контактов
     function showAdressBook() {
-        //check if the key addbook exist in local storage of else create it
-        //if it exist, load content from the local storage and loop - display it on the page
         if (localStorage['addbook'] === undefined) {
             localStorage['addbook'] = "[]";
         } else {
@@ -114,6 +112,90 @@ window.onload = function () {
                 addBookDiv.innerHTML += str;}
         }
     }
+	//валидация firstName
+	function valName(){
+		var errName1 = document.querySelector(".nameError1")
+		if (firstName.value !=0){
+			errName1.style.display = "none";
+			firstName.style.border = "1px solid black";
+			return true
+		}
+		else{
+			console.log("hui")
+			errName1.style.display = "inline-block";
+			firstName.style.border = "2px solid red";
+			return false
+		}
+	}
+	
+	//валидация secondName
+	function valSName(){
+		var errName1 = document.querySelector(".nameError2")
+		if (secondName.value !=0){
+			errName1.style.display = "none";
+			secondName.style.border = "1px solid black";
+			return true
+		}
+		else{
+			console.log("hui")
+			errName1.style.display = "inline-block";
+			secondName.style.border = "2px solid red";
+			return false
+		}
+	}
+	
+	//валидация phone
+	function valPhone(){
+		var errPhone = document.querySelector(".phoneError")
+		var errPhone1 = document.querySelector(".phoneError1")
+		if (phone.value.length == 12){
+			errPhone.style.display = "none";
+			errPhone1.style.display = "none";
+			phone.style.border = "1px solid black";
+			return true
+		}
+		else if(phone.value.length != 12 && phone.value.length !=0 ){
+			console.log(phone.value.length)
+			errPhone1.style.display = "inline-block";
+			phone.style.border = "2px solid red";
+			errPhone.style.display = "none";
+			return false
+		}else if (phone.value.length == 0){
+			errPhone.style.display = "inline-block";
+			phone.style.border = "2px solid red";
+			errPhone1.style.display = "none";
+			return false
+	}
+	}
+	
+	//валидация email
+	function valEmail(){
+		var emEr1 = document.querySelector(".emailError");
+		var emEr2 = document.querySelector(".emailError1");
+		var emResult = email.value.match('^[-0-9a-zA-Z.+_]+@[-0-9a-zA-Z.+_]+\.[a-zA-Z]{2,4}$');
+		console.log(emResult);
+		if(emResult == null){
+			if (email.value.length == 0){
+				console.log(1);
+				email.style.border = "2px solid red";
+				emEr1.style.display = "inline-block";
+				emEr2.style.display = "none";
+			}else{
+				console.log(2);
+				email.style.border = "2px solid red";
+				emEr2.style.display = "inline-block";
+				emEr1.style.display = "none";
+			}
+		}else{	
+				console.log(3);
+				email.style.border = "1px solid black";
+				emEr2.style.display = "none";
+				emEr1.style.display = "none";
+				return true
+		}
+	}
+	
+		//поиск по имени контакта
 			$(document).ready(function() {
 				$('#search').on('keyup', function() {
 				var oko = $(this).val().toUpperCase();
